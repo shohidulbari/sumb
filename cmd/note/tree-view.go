195a1516e93381e343961fcca71fb31cb8ac84a3
shohidulbari <shohidulbari18@gmail.com>
2025-10-31 17:58:18 +0600

    Add text formatter in tree-view

--- cmd/note/tree-view.go
+++ cmd/note/tree-view.go
@@ -5,22 +5,45 @@ import (
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/tree"
+	"github.com/muesli/reflow/wordwrap"
 	"github.com/shohidulbari/sumb/db"
 )
 
+var (
+	NOTE_BODY_TAKE_LIMIT = 200
+	HIGHLIGHT_COLOR      = lipgloss.Color("63")
+)
+
+func formatNoteBody(body string) string {
+	if (len(body)) > NOTE_BODY_TAKE_LIMIT {
+		remainingCharacters := len(body) - NOTE_BODY_TAKE_LIMIT
+		take := body[:NOTE_BODY_TAKE_LIMIT]
+		wrapped := wordwrap.String(take, 50)
+
+		messageStyle := lipgloss.NewStyle().Foreground(HIGHLIGHT_COLOR).Italic(true)
+		message := messageStyle.Render(
+			fmt.Sprintf("... (truncated, %d more characters)", remainingCharacters),
+		)
+		return fmt.Sprintf(
+			"%s... %s",
+			wrapped,
+			message,
+		)
+	}
+	return wordwrap.String(body, 50)
+}
+
 func RenderTreeView(root string, notes []db.NoteResponse) *tree.Tree {
-	enumeratorStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("63")).MarginRight(1)
-	rootStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("63"))
-	itemStyle := lipgloss.NewStyle().Width(50)
+	enumeratorStyle := lipgloss.NewStyle().Foreground(HIGHLIGHT_COLOR).MarginRight(1)
+	rootStyle := lipgloss.NewStyle().Foreground(HIGHLIGHT_COLOR).Bold(true)
 	t := tree.Root(root)
 
 	for _, note := range notes {
-		noteTree := tree.New().Root(fmt.Sprintf("[%s]", note.ID)).Child(note.Body)
+		noteTree := tree.New().Root(fmt.Sprintf("[%s]", note.ID)).Child(formatNoteBody(note.Body))
 		t = t.Child(noteTree)
 	}
 	t = t.Enumerator(tree.RoundedEnumerator).
 		EnumeratorStyle(enumeratorStyle).
-		RootStyle(rootStyle).
-		ItemStyle(itemStyle)
+		RootStyle(rootStyle)
 	return t
 }
